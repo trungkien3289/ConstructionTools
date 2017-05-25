2af235174591f92e13377ff9d64f3908b3499778
sinhvv <sinhvv@rikkeisoft.com>
2017-05-25 17:31:33 +0700

    fix responsive layout

--- OnlineStoreMVC/Scripts/DisplayProduct/HomePage.js
+++ OnlineStoreMVC/Scripts/DisplayProduct/HomePage.js
@@ -26,7 +26,7 @@
                     loop: true,
                     margin: 10,
                     autoplay: true,
-                    autoplayTimeout: 1000,
+                    autoplayTimeout: 2000,
                     autoplayHoverPause: true,
                     //responsiveClass: true,
                     responsive: {
