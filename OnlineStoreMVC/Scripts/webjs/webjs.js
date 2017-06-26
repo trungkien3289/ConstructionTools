1c4e73f8f2952cd89c39c35efd9677477ea62b31
thurstan <thurstan@enclave.vn>
2017-06-26 15:12:38 +0700

    added web.js file

---
+++ OnlineStoreMVC/Scripts/webjs/webjs.js
@@ -0,0 +1,16 @@
+﻿//--Function set height for header
+function setHeightForHeaderAndMenu() {
+    var headerHeight = $(".header-bot").innerHeight();
+    var menuHeight = $(".ban-top").innerHeight();
+    var totalHeight = headerHeight + menuHeight;
+
+    $(".header_menu_container").height(totalHeight);
+}
+
+$(document).ready(function () {
+    setHeightForHeaderAndMenu();
+})
+
+$(window).resize(function () {
+    setHeightForHeaderAndMenu();
+})
